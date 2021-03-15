71444050ceb2dbeb5d9ac2b678ff523fba12484b
yusufkarabay <yusufkarabay21@gmail.com>
2021-03-15 20:45:09 +0300

    Corporation class created

---
+++ NobetTakip/NobetTakip/NobetTakip/Models/Corporation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NobetTakip.Models
+{
+    public class Corporation
+    {
+        public string CorporationName;
+        public string CorporationCode;
+
+    }
+}
