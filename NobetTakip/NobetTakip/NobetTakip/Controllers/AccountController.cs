7213f1a6468773fb3a020126ea9a2f679b8928f5
yusufkarabay <yusufkarabay21@gmail.com>
2021-03-15 20:14:03 +0300

    Yayınlama

---
+++ NobetTakip/NobetTakip/NobetTakip/Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NobetTakip.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Authorization;
+
+namespace NobetTakip.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            //User user = new User() { MailAddress = "yusufkarabay21@gmail.com", Password = "süleymansüleyman" };
+            return View(new User());
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login(User userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                if (userModel.MailAddress == "yusufkarabay21@gmail.com" && userModel.Password == "123456")
+                {
+                    return RedirectToAction("Index", "Home");
+                } else
+                {
+                    return RedirectToAction("Error", "Home");
+                }
+            }
+
+            return View(userModel);
+        }
+    }
+}
---
+++ NobetTakip/NobetTakip/NobetTakip/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NobetTakip.Models
+{
+    public class User // personel diye düşünebiliriz
+    {
+
+        public string RealName;
+        public string Username { get; set; }
+        public string MailAddress { get; set; }
+        public string GSMNo;
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
+        public bool IsAdmin; // yönetici mi değil mi ?
+    }
+}
